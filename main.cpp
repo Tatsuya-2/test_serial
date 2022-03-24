f691f5c7969dd50cad0e51315ef83f301a2d8f4d
Tatsuya-2 <ttjoker02@gmail.com>
2022-03-24 10:50:08 +0900

    Update: multithread sample

--- main.cpp
+++ main.cpp
@@ -1,111 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdint>
 #include <iostream>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <fcntl.h>
-#include <sys/signal.h>
-#include <errno.h>
-#include <termios.h>
-/* https://stackoverflow.com/questions/15119412/setting-serial-port-interruption-in-linux */
+#include <mutex>
+#include <thread>
 
-void signal_handler_IO(int status); /* definition of signal handler */
+std::mutex mtx_;  // 排他制御用ミューテックス
+uint32_t count_;
 
-int n;
-int fd;
-int connected;
-struct termios termAttr;
-struct sigaction saio;
-
-int main(int argc, char* argv[])
+void add_count()
 {
-  fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY);
-  if (fd == -1)
-  {
-    perror("open_port: Unable to open /dev/ttyO1\n");
-    exit(1);
-  }
-
-  saio.sa_handler = signal_handler_IO;
-  saio.sa_flags = 0;
-  saio.sa_restorer = NULL;
-  sigaction(SIGIO, &saio, NULL);
-
-  fcntl(fd, F_SETFL, FNDELAY);
-  fcntl(fd, F_SETOWN, getpid());
-  fcntl(fd, F_SETFL, O_ASYNC);
-
-  tcgetattr(fd, &termAttr);
-  cfsetispeed(&termAttr, B115200);
-  cfsetospeed(&termAttr, B115200);
-  termAttr.c_cflag &= ~PARENB;
-  termAttr.c_cflag &= ~CSTOPB;
-  termAttr.c_cflag &= ~CSIZE;
-  termAttr.c_cflag |= CS8;
-  termAttr.c_cflag |= (CLOCAL | CREAD);
-  termAttr.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-  termAttr.c_iflag &= ~(IXON | IXOFF | IXANY);
-  termAttr.c_oflag &= ~OPOST;
-  tcsetattr(fd, TCSANOW, &termAttr);
-  printf("UART1 configured....\n");
-
-  connected = 1;
-  while (connected == 1)
-  {
-    // write(fd, "Waiting for data...\n", 20);
-    printf("Waiting for data...\n");
-    sleep(1);
-  }
-
-  close(fd);
-  exit(0);
+  // count_を加算する前にミューテックスを取得する
+  std::lock_guard<std::mutex> lock(mtx_);
+  ++count_;
 }
 
-int spot = 0;
-char buf = '\0';
-
-/* Whole response*/
-char response[256];
-unsigned char buffer[256];
-
-void signal_handler_IO(int status)
+void ThreadA()
 {
-  printf("received data from UART.\n");
-  int len;
-  do
+  for (int i = 0; i < 100000; ++i)
   {
-    len = read(fd, response, 256);
+    add_count();
+  }
+}
 
-    printf("%s", response);
-  } while (len > 0);
+void ThreadB()
+{
+  for (int i = 0; i < 100000; ++i)
+  {
+    add_count();
+  }
+}
 
-  // do
-  // {
-  //   n = read(fd, &buf, 1);
-  //   sprintf(&response[spot], "%c", buf);
-  //   spot += n;
-  // } while (buf != '\r' && n > 0);
+int main()
+{
+  count_ = 0;
 
-  // if (n < 0)
-  // {
-  //   std::cout << "Error reading: " << strerror(errno) << std::endl;
-  // }
-  // else if (n == 0)
-  // {
-  //   std::cout << "Read nothing!" << std::endl;
-  // }
-  // else
-  // {
-  //   std::cout << "Response: " << response << std::endl;
-  // }
+  std::thread th_a(ThreadA);
+  std::thread th_b(ThreadB);
 
-  // n = spot = 0;
-  // /// zero padding response
-  // for (int i = 0; i < 256; i++)
-  // {
-  //   response[i] = '\0';
-  // }
+  th_a.join();
+  th_b.join();
+
+  std::cout << "count_ : " << count_ << std::endl;
+
+  return 0;
 }
